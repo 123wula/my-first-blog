5e0fbcfdc428fdea4e7b1144db279c9e2c3712b8
123wula <bhybhy1225@163.com>
2025-05-29 05:29:59 +0000

    合并生产环境配置

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "mysite.urls"
@@ -117,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
